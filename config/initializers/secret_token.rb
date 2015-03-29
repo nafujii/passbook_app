25f8adbcaf014cbab5767d9c7fd6ea0bd310ac26
nafujii <dev@nfujii.com>
2015-03-30 00:12:15 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PassbookApp::Application.config.secret_key_base = 'dae25a9eb09827b908e6796f4b7bc07e366e5fd931b835da63fea5f00f0749c6d8de4e743abe5715aa69832ddee6cce5fbd662424a3fa6c6216aae4e764d5dfe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PassbookApp::Application.config.session_store :cookie_store, key: '_passbook_app_session'
